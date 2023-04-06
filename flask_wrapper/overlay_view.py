2fdf16e0267ebdcd49d05fa6b53df7dca83150b5
jlnk03 <95719434+jlnk03@users.noreply.github.com>
2023-04-06 12:12:47 +0300

    ui report

--- flask_wrapper/overlay_view.py
+++ flask_wrapper/overlay_view.py
@@ -8,34 +8,34 @@ overlay = html.Div(
             className='fixed top-24 w-full bottom-0 dark:bg-slate-700 bg-[#FAF7F5] rounded-t-3xl',
             children=[
                 html.Div(
-                    className='relative flex flex-col gap-4 mx-6 my-6 overflow-y-auto overflow-x-hidden h-full',
+                    className='relative h-full flex flex-col gap-4 ml-6 py-6 overflow-x-hidden',
                     children=[
                         html.Div(
-                            className='w-full h-fit flex flex-col gap-4',
+                            className='w-full h-full flex flex-col gap-4 overflow-y-auto',
                             children=[
                                 html.Div('Report',
-                                         className='text-3xl text-center font-bold mt-4 mb-10 text-gray-900 dark:text-gray-100'),
+                                         className='sm:text-3xl text-xl text-left font-bold mt-4 sm:mb-10 mb-6 text-gray-900 dark:text-gray-100'),
 
                                 html.Div(className='flex flex-row',
                                          children=[
-                                             html.Canvas(id='setup_frame', className='rounded-2xl'),
+                                             html.Canvas(id='setup_frame', className='rounded-2xl max-h-56 sm:max-h-96'),
                                              html.Div(
                                                  className='flex flex-col ml-6',
                                                  children=[
                                                      html.Span('Setup',
-                                                               className='text-2xl text-left font-bold mb-8 text-gray-900 dark:text-gray-100'),
+                                                               className='sm:text-2xl text-lg text-left font-bold mb-4 text-gray-900 dark:text-gray-100 sm:mb-8 mb-6'),
                                                      html.Span('Pelvis',
-                                                               className='text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
+                                                               className='sm:text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
                                                      html.Span(id='pelvis_report_text',
-                                                               className='text-base text-left text-gray-900 dark:text-gray-100 mb-4'),
+                                                               className='sm:text-base text-sm text-left text-gray-900 dark:text-gray-100 mb-4'),
                                                      html.Span('Thorax',
-                                                               className='text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
+                                                               className='sm:text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
                                                      html.Span(id='thorax_report_text',
-                                                               className='text-base text-left text-gray-900 dark:text-gray-100 mb-4'),
+                                                               className='sm:text-base text-sm text-left text-gray-900 dark:text-gray-100 mb-4'),
                                                      html.Span('Head',
-                                                               className='text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
+                                                               className='sm:text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
                                                      html.Span(id='head_report_text',
-                                                               className='text-base text-left text-gray-900 dark:text-gray-100'),
+                                                               className='sm:text-base text-sm text-left text-gray-900 dark:text-gray-100 mb-4'),
                                                  ]
                                              )
                                          ]
@@ -43,49 +43,49 @@ overlay = html.Div(
 
                                 html.Div(className='flex flex-row',
                                          children=[
-                                             html.Canvas(id='top_frame', className='rounded-2xl'),
+                                             html.Canvas(id='top_frame', className='rounded-2xl max-h-56 sm:max-h-96'),
                                              html.Div(
                                                  className='flex flex-col ml-6',
                                                  children=[
                                                      html.Span('Top',
-                                                               className='text-2xl text-left font-bold mb-4 text-gray-900 dark:text-gray-100 mb-8'),
+                                                               className='sm:text-2xl text-lg text-left font-bold mb-4 text-gray-900 dark:text-gray-100 sm:mb-8 mb-6'),
                                                      html.Span('Pelvis',
-                                                               className='text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
+                                                               className='sm:text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
                                                      html.Span(id='pelvis_report_text_top',
-                                                               className='text-base text-left text-gray-900 dark:text-gray-100 mb-4'),
+                                                               className='sm:text-base text-sm text-left text-gray-900 dark:text-gray-100 mb-4'),
                                                      html.Span('Thorax',
-                                                               className='text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
+                                                               className='sm:text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
                                                      html.Span(id='thorax_report_text_top',
-                                                               className='text-base text-left text-gray-900 dark:text-gray-100 mb-4'),
+                                                               className='sm:text-base text-sm text-left text-gray-900 dark:text-gray-100 mb-4'),
                                                      html.Span('Head',
-                                                               className='text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
+                                                               className='sm:text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
                                                      html.Span(id='head_report_text_top',
-                                                               className='text-base text-left text-gray-900 dark:text-gray-100 mb-4'),
+                                                               className='sm:text-base text-sm text-left text-gray-900 dark:text-gray-100 mb-4'),
                                                  ]
                                              )
                                          ]
                                          ),
 
-                                html.Div(className='flex flex-row gap-4',
+                                html.Div(className='flex flex-row',
                                          children=[
-                                             html.Canvas(id='impact_frame', className='rounded-2xl'),
+                                             html.Canvas(id='impact_frame', className='rounded-2xl max-h-56 sm:max-h-96'),
                                              html.Div(
                                                  className='flex flex-col ml-6',
                                                  children=[
                                                      html.Span('Impact',
-                                                               className='text-2xl text-left font-bold mb-4 text-gray-900 dark:text-gray-100 mb-8'),
+                                                               className='sm:text-2xl text-lg text-left font-bold mb-4 text-gray-900 dark:text-gray-100 sm:mb-8 mb-6'),
                                                      html.Span('Pelvis',
-                                                               className='text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
+                                                               className='sm:text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
                                                      html.Span(id='pelvis_report_text_impact',
-                                                               className='text-base text-left text-gray-900 dark:text-gray-100 mb-4'),
+                                                               className='sm:text-base text-sm text-left text-gray-900 dark:text-gray-100 mb-4'),
                                                      html.Span('Thorax',
-                                                               className='text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
+                                                               className='sm:text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
                                                      html.Span(id='thorax_report_text_impact',
-                                                               className='text-base text-left text-gray-900 dark:text-gray-100 mb-4'),
+                                                               className='sm:text-base text-sm text-left text-gray-900 dark:text-gray-100 mb-4'),
                                                      html.Span('Head',
-                                                               className='text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
+                                                               className='sm:text-xl text-left font-medium text-gray-900 dark:text-gray-100 mb-2'),
                                                      html.Span(id='head_report_text_impact',
-                                                               className='text-base text-left text-gray-900 dark:text-gray-100 mb-4'),
+                                                               className='sm:text-base text-sm text-left text-gray-900 dark:text-gray-100 mb-4'),
                                                  ]
                                              )
                                          ]
