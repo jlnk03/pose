f30262016830b9fce2f631b14de2bb19968e69e1
jlnk03 <95719434+jlnk03@users.noreply.github.com>
2023-04-12 01:32:11 +0200

    menu button

--- flask_wrapper/assets/menu.js
+++ flask_wrapper/assets/menu.js
@@ -1,12 +1,14 @@
-// let button = document.getElementById("close_menu")
-// let button_open = document.getElementById("open_menu")
-// let sidebar = document.getElementById("menubar");
-//
-// button.addEventListener("click", function() {
-//     sidebar.style.display = "none";
-// });
-//
-// button_open.addEventListener("click", function() {
-//     sidebar.style.display = "block";
-//     // sidebar.style.transition = "opacity 0.5s";
-// })
+//  Show menu outside dashboard
+
+let button = document.getElementById("close_menu")
+let button_open = document.getElementById("open_menu")
+let sidebar = document.getElementById("menubar");
+
+button.addEventListener("click", function () {
+    sidebar.style.display = "none";
+});
+
+button_open.addEventListener("click", function () {
+    sidebar.style.display = "block";
+    // sidebar.style.transition = "opacity 0.5s";
+})
